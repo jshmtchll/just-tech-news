ba16191ea8e26f883f60ba4f555f6cbb915b50bd
josh mitchell <jshmtchll@gmail.com>
2021-05-05 12:52:33 -0500

    password ecryption and pw verify

--- models/User.js
+++ models/User.js
@@ -2,8 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-//CREATE USER MODEL
-class User extends Model {}
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
 
 //DEFINE TABLE COLUMNS AND CONFIGS
 User.init(
