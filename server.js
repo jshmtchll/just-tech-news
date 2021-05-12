244b4c4d5b9ada78e4a9a856953ad4429c7b5ece
josh mitchell <jshmtchll@gmail.com>
2021-05-12 16:24:36 -0500

    added session cokkies and login/logout routes

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-const path = require('path');
-const exphbs = require('express-handlebars');
+const path = require("path");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: process.env.Secret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
